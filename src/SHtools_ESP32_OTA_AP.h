31ee57a14022fa3d73e84b901e5d82c271132f11
Shafick Cruz <shafick@outlook.com.br>
2024-09-26 09:25:49 +0100

    2.0.0

--- src/SHtools_ESP32_OTA_AP.h
+++ src/SHtools_ESP32_OTA_AP.h
@@ -31,7 +31,7 @@ private:
     String nomeSketch;
     unsigned long ota_progress_millis;
     AsyncWebServer server; // Declaração do servidor como membro da classe
-    // static const char IndexHTML[] PROGMEM; // Declaração da página HTML
+    static const char IndexHTML[] PROGMEM; // Declaração da página HTML
 
     void led_handle();
     void bt_handle();
