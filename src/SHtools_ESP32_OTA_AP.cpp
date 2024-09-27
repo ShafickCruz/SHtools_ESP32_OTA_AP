f72ad4ec9560cd5d0a041f179924d8024cff930e
Shafick Cruz <shafick@outlook.com.br>
2024-09-27 04:51:16 +0100

    2.1.0

--- src/SHtools_ESP32_OTA_AP.cpp
+++ src/SHtools_ESP32_OTA_AP.cpp
@@ -127,6 +127,7 @@ void SHtools_ESP32_OTA_AP::bt_handle()
 
 void SHtools_ESP32_OTA_AP::startServerMode()
 {
+    ServerMode = true;
     Serial.println("Entrando em modo Servidor...");
 
     WifiSetup();               // configura e inicializa o servidor wifi
@@ -135,12 +136,14 @@ void SHtools_ESP32_OTA_AP::startServerMode()
     WebSerial.begin(&server);  // Start Webserial
     server.begin();            // Start webserver
 
-    ServerMode = true;
     Serial.println("Servidor iniciado");
 }
 
 void SHtools_ESP32_OTA_AP::rotasEcallbacks()
 {
+    if (!ServerMode) // se nao estiver em modo servidor, impede que entradas na serial seja processadas
+        return;
+
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
               { request->send(200, "text/html", IndexHTML); });
 
